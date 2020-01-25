9511026968bccecb4b335c893b316325ff0fc2d3
Caique Pires <cfapires@gmail.com>
2020-01-25 20:37:39 -0300

    Commit Inicial

---
+++ src/main/java/com/dev/bemba/api/CasaGrenaApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.bemba.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaGrenaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaGrenaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/bemba/CasaGrenaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.bemba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaGrenaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
